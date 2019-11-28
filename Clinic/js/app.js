957b10d0362529f38eb3cda91ba653f7c5f86b2f
dushenko <dushenko1992@gmail.com>
2019-11-28 03:17:30 +0200

    fix menu

--- Clinic/js/app.js
+++ Clinic/js/app.js
@@ -3,6 +3,54 @@
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
+  $('.js-terminal-input').focus(function () {
+    $('.keyboard').addClass('active');
+    $('.statementForm__field').removeClass('active');
+    $(this).parent().addClass('active');
+  });
+  $('.js-confirm').click(function () {
+    $('.js-keyboard').removeClass('active');
+    $('.statementForm__field').removeClass('active');
+  });
+
+  if ($('.js-keyboard').length) {
+    var v = $('.statementForm__field.active input').val();
+    var studentCard = $('#card-number');
+    var policeNumber = $('#police-number');
+    $('.numeral').on('click', function () {
+      var v = $('.statementForm__field.active input').val();
+      console.log(v);
+
+      if (v.length > 15) {
+        return;
+      }
+
+      var value = $(this).data('value');
+      console.log();
+      $('.statementForm__field.active input').val(v + value);
+    });
+  }
+
+  $(".erasing").click(function () {
+    var el = $(".statementForm__field.active input");
+    var the_value = el.val();
+    the_value = the_value.substring(0, the_value.length - 1);
+    el.val(the_value);
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  $('.js-tetminal-choose').click(function () {
+    $('.js-mask').addClass('active');
+  });
+  $('.choose-data-close').click(function () {
+    $('.js-mask').removeClass('active');
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
   var headers = document.querySelectorAll('.js-accordion .js-title');
 
   for (var i = 0; i < headers.length; i++) {
