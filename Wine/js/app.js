9cdf0134ce82ecdbe7ef04a94bc12724fddfb490
dushenko <dushenko1992@gmail.com>
2019-11-29 03:36:37 +0200

    fix bugs v_4

--- Wine/js/app.js
+++ Wine/js/app.js
@@ -41,14 +41,22 @@ $(document).ready(function () {
   $('.scroll-up').on('click', function () {
     fullpage_api.moveSectionUp();
   });
-
   $('.js-form').on('click', function () {
     $('.js-mask').fadeIn('active');
     $('.js-modal').fadeIn('active');
   });
-
   $('.js-mask').on('click', function () {
     $(this).fadeOut('active');
     $('.js-modal').fadeOut('active');
+  });
+
+  if ($(window).width() < 1024) {
+    fullpage_api.setResponsive(true);
+  }
+
+  $(window).resize(function () {
+    if ($(window).width() < 1024) {
+      fullpage_api.setResponsive(true);
+    }
   });
 });
