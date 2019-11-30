c9793bd84b3eb26b0a0247630ba18bf99905720b
dushenko <dushenko1992@gmail.com>
2019-11-30 13:24:13 +0200

    add desktop LK 040в

--- Moscow/js/app.js
+++ Moscow/js/app.js
@@ -1,6 +1,21 @@
 "use strict";
 
-//=require ../blocks/**/*.js
 document.addEventListener('DOMContentLoaded', function () {
   'use strict';
+
+  $('.js-open-modal').click(function () {
+    $('.js-mask').fadeIn();
+    $('.js-modal-form').fadeIn();
+  });
+  $('.js-modal-close').click(function () {
+    $('.js-mask').fadeOut();
+    $('.modal').fadeOut();
+  });
+  $('.js-thanks').click(function () {
+    $('.js-modal-form').fadeOut();
+    $('.js-modal-thanks').fadeIn();
+  });
+  $('.form__input').focus(function () {
+    $('.form__input-wrap').addClass('active');
+  });
 });
