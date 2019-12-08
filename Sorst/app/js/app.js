f93e2a05c8f1f75db69eeda59d5501127a89e43b
dushenko <dushenko1992@gmail.com>
2019-12-08 17:17:10 +0200

    add sorst

---
+++ Sorst/app/js/app.js
@@ -0,0 +1,5 @@
+//=require ../blocks/**/*.js
+$(document).ready(function () {
+
+
+});
---
+++ Sorst/build/js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.js-prod-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    responsive: [{
+      breakpoint: 3000,
+      settings: "unslick"
+    }, {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+});
+$(document).ready(function () {});
