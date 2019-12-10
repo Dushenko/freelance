39d6c31638dd23cfc1d9a6992c7755397104ce24
dushenko <dushenko1992@gmail.com>
2019-12-10 13:42:08 +0200

    add fix header

--- Sorst/app/js/app.js
+++ Sorst/app/js/app.js
@@ -1,12 +1,26 @@
 //=require ../blocks/**/*.js
 $(document).ready(function () {
 
-	$('.js-burger').click( function () {
-		$(this).toggleClass('active');
-		$('.js-nav').toggleClass('active');
-		$('html').toggleClass('overflow');
-		$('body').toggleClass('overflow');
-	});
+	// $('.js-burger').click( function () {
+	// 	$(this).toggleClass('active');
+	// 	$('.js-nav').toggleClass('active');
+	// 	$('html').toggleClass('overflow');
+	// 	$('body').toggleClass('overflow');
+	// });
+
+	if(document.querySelector('.js-burger')){
+		$('.js-burger').click(function(){
+			if($(this).hasClass('active')){
+				$('body').removeClass('overflow');
+				$(this).removeClass('active');
+				$('.js-nav').removeClass('active');
+			} else {
+				$('body').addClass('overflow');
+				$(this).addClass('active');
+				$('.js-nav').addClass('active');
+			}
+		})
+	}
 
 	$('.js-lang').click( function () {
 		$('.langs').slideToggle('active');
