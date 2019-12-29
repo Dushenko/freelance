7060c3f74b73c6a5ceaee12575e33441b0953fde
dushenko <dushenko1992@gmail.com>
2019-12-30 01:27:01 +0200

    add system pharm v1

---
+++ SystemPharm/app/blocks/header/header.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	//burger
+	$('.js-burger').click( function () {
+		$(this).toggleClass('active');
+		$('.js-nav').toggleClass('active');
+	});
+
+});
---
+++ SystemPharm/build/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function () {
+  //burger
+  $('.js-burger').click(function () {
+    $(this).toggleClass('active');
+    $('.js-nav').toggleClass('active');
+  });
+});
+$(document).ready(function () {});
