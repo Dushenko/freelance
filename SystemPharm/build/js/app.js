0d2d81da28363f8549b5f9904c289bb58fc4a39c
dushenko <dushenko1992@gmail.com>
2020-01-08 10:33:01 +0200

    add project в

--- SystemPharm/build/js/app.js
+++ SystemPharm/build/js/app.js
@@ -19,6 +19,43 @@ $(document).ready(function () {
   }
 });
 $(document).ready(function () {
+  //modal 
+  $('.js-open-modal').click(function () {
+    $('.js-contacts').fadeIn();
+    $('.js-mask').fadeIn();
+  });
+  $('.js-modal-close').click(function () {
+    $('.modal').fadeOut();
+    $('.js-mask').fadeOut();
+  });
+  $('.js-mask').click(function () {
+    $(this).fadeOut();
+    $('.modal').fadeOut();
+  });
+});
+$(document).ready(function () {
+  //select 
+  $('.js-select').click(function () {
+    $(this).toggleClass('active');
+    $('.js-select-list').slideToggle();
+  }); //close custom select
+
+  $(function ($) {
+    $(document).mouseup(function (e) {
+      var div = $(".js-select");
+
+      if (!div.is(e.target) && div.has(e.target).length === 0) {
+        div.removeClass('active');
+        $('.js-select-list').slideUp();
+      }
+    });
+  });
+  $('.select__list li').click(function () {
+    var selected = $(this).text();
+    $('.select__text').text(selected);
+  });
+});
+$(document).ready(function () {
   $('.js-vacancy-slider').slick({
     slidesToShow: 5,
     slidesToScroll: 1,
