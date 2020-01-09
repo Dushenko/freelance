a22c152d35da071c31f7b85b54720053e8c7b6ff
dushenko <dushenko1992@gmail.com>
2020-01-09 08:28:39 +0200

    beauty 2

--- Beauty/build/js/app.js
+++ Beauty/build/js/app.js
@@ -22,4 +22,16 @@ $(document).ready(function () {
     $(this).parent().parent().parent().find('.select__text').text(selected);
   });
 });
+$(document).ready(function () {
+  //tabs
+  $(function () {
+    $("ul.js-tab-btn").on("click", "li:not(.active)", function () {
+      $(this).addClass("active").siblings().removeClass("active").closest("div.js-tabs").find("div.js-tab-content").removeClass("active").eq($(this).index()).addClass("active");
+    });
+  });
+  $('.js-tab-btn li a').click(function () {
+    var selected = $(this).text();
+    $('.select-text').text(selected);
+  });
+});
 $(document).ready(function () {});
