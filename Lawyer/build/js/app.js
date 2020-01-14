8e48496618c91d1490f126063bd00396be1ff993
dushenko <dushenko1992@gmail.com>
2020-01-15 01:15:46 +0200

    add advokat V2

--- Lawyer/build/js/app.js
+++ Lawyer/build/js/app.js
@@ -1,6 +1,25 @@
 "use strict";
 
-//=require ../blocks/**/*.js
+$(document).ready(function () {
+  //burger
+  if (document.querySelector('.js-burger')) {
+    $('.js-burger').click(function () {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $('.js-nav').removeClass('active');
+        $('.main').removeClass('opacity');
+        $('body').removeClass('overflow');
+        $('html').removeClass('overflow');
+      } else {
+        $(this).addClass('active');
+        $('.js-nav').addClass('active');
+        $('.main').addClass('opacity');
+        $('body').addClass('overflow');
+        $('html').addClass('overflow');
+      }
+    });
+  }
+});
 $(document).ready(function () {
   //lang
   $(".lang__link").click(function () {
