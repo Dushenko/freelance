da9187404bad499624992fd1e73c3a3a7fb8bcd2
dushenko <dushenko1992@gmail.com>
2020-01-15 02:02:19 +0200

    add advokat V3

--- Lawyer/app/blocks/header/header.js
+++ Lawyer/app/blocks/header/header.js
@@ -6,13 +6,11 @@ $(document).ready(function () {
 			if($(this).hasClass('active')){
 				$(this).removeClass('active');
 				$('.js-nav').removeClass('active');
-				$('.main').removeClass('opacity');
 				$('body').removeClass('overflow');
 				$('html').removeClass('overflow');
 			} else {
 				$(this).addClass('active');
 				$('.js-nav').addClass('active');
-				$('.main').addClass('opacity');
 				$('body').addClass('overflow');
 				$('html').addClass('overflow');
 			}
