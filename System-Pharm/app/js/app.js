4764ce8c08afc578fbdde646353379ec108666a2
dushenko <dushenko1992@gmail.com>
2020-01-21 10:42:10 +0200

    add animation fix 2

--- System-Pharm/app/js/app.js
+++ System-Pharm/app/js/app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
 
 
-const CANVAS = document.getElementsByTagName("canvas")[0],
+const CANVAS = document.getElementsByClassName("canvasSpace")[0],
 	CTX = CANVAS.getContext("2d"),
 	W = window.innerWidth,
 	H = window.innerHeight,
@@ -110,111 +110,125 @@ init();
 
 
 
+//canvasNeuron
 
+// (function () {
+// 	'use strict';
+	
+// 	var canvas,
+//       	ctx,
+//       	mousePos,
+//       	points = [],
+//       	maxDist = 200,
+//       	colour = "2,96,155";
 
-// "use strict";
+// 	function init () {
+// 		//Add on load scripts
+// 		canvas = document.getElementById("canvas");
+// 		ctx = canvas.getContext("2d");
+// 		canvas.addEventListener("mousemove",function(evt){
+// 			mousePos = getMousePos(canvas,evt);
+// 		});
+// 		resizeCanvas();
+// 		generatePoints(150);
+// 		pointFun();
+// 		setInterval(pointFun,6);
+// 		window.addEventListener('resize', resizeCanvas, false);
+// 	}
+// 	//Particle constructor
+// 	function point () {
+// 		this.x = (Math.random()*(canvas.width+maxDist))-(maxDist/2);
+// 		this.y = (Math.random()*(canvas.height+maxDist))-(maxDist/2);
+// 		this.vx = (Math.random()*1)-.5;
+// 		this.vy = (Math.random()*1)-.5;
+//     this.dia = Math.random()*3+1;
+// 		points.push(this);
+// 	}
+// 	//Point generator
+// 	function generatePoints (amount) {
+// 		var temp;
+// 		for (var i = 0; i < amount; i++) {
+// 			temp = new point();
+// 		};
+// 		console.log(points);
+// 	}
+// 	//Point drawer
+// 	function draw (obj) {
+// 		ctx.beginPath();
+// 		ctx.fillStyle = "rgb("+colour+")";
+// 		if(obj.dia){
+//       ctx.arc(obj.x,obj.y,obj.dia,0,2*Math.PI);
+//     }else{
+//       ctx.arc(obj.x,obj.y,2,0,2*Math.PI);
+//     }
+// 		ctx.closePath();
+// 		ctx.fill();
+// 		//ctx.stroke();
+// 	}
+// 	//Updates point position values
+// 	function update (obj) {
+// 		obj.x += obj.vx;
+// 		obj.y += obj.vy;
+// 		if (obj.x > canvas.width+(maxDist/2)) {
+// 			obj.x = -(maxDist/2);
+// 		}
+// 		else if (obj.xpos < -(maxDist/2)) {
+// 			obj.x = canvas.width+(maxDist/2);
+// 		}
+// 		if (obj.y > canvas.height+(maxDist/2)) {
+// 			obj.y = -(maxDist/2);
+// 		}
+// 		else if (obj.y < -(maxDist/2)) {
+// 			obj.y = canvas.height+(maxDist/2);
+// 		}
+// 	}
+// 	//
+// 	function pointFun () {
+// 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+// 		if (mousePos) {
+// 			collision(mousePos,maxDist*2);
+// 			draw(mousePos);
+// 		}
+// 		for (var i = 0; i < points.length; i++) {
+// 			collision(points[i],maxDist);
+//       draw(points[i]);
+// 			update(points[i]);
+// 		};
+// 	}
 
-// var canvas = document.getElementById('canvas'),
-//   ctx = canvas.getContext('2d'),
-//   w = canvas.width = window.innerWidth,
-//   h = canvas.height = window.innerHeight,
-    
-//   hue = 217,
-//   stars = [],
-//   count = 0,
-//   maxStars = 600;
+// 	function collision (obj,dist) {
+// 		var temp;
+// 		for (var i = 0; i < points.length; i++) {
+// 			//Don't interact with self
+// 			if (obj !== points[i]) {
+// 				temp = Math.sqrt(Math.pow((obj.x - points[i].x),2) + Math.pow((obj.y - points[i].y),2));
+// 				if (temp<dist) {
+// 					ctx.beginPath();
+// 					ctx.moveTo(obj.x, obj.y);
+// 					ctx.strokeStyle = "rgba("+colour+","+.8*Math.pow((dist-temp)/dist,5)+")";
+//           ctx.moveTo(obj.x, obj.y);
+// 					ctx.lineTo(points[i].x, points[i].y);
+// 					ctx.closePath();
+// 					ctx.stroke();
+// 				};
+// 			}
+// 		};
+// 	}
 
-// var canvas2 = document.createElement('canvas'),
-//     ctx2 = canvas2.getContext('2d');
-//     canvas2.width = 2000;
-//     canvas2.height = 2000;
-// var half = canvas2.width/2,
-//     gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-//     gradient2.addColorStop(0.025, '#fff');
-//     gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
-//     gradient2.addColorStop(0.35, 'hsl(' + hue + ', 64%, 6%)');
-//     gradient2.addColorStop(1, 'transparent');
+// 	function resizeCanvas() {
+// 		canvas.width = window.innerWidth;
+// 	canvas.height = window.innerHeight;
+// 		pointFun();
+// 	}
 
-//     ctx2.fillStyle = gradient2;
-//     ctx2.beginPath();
-//     ctx2.arc(half, half, half, 0, Math.PI * 2);
-//     ctx2.fill();
+// 	function getMousePos(cvs, evt1) {
+// 		var rect = cvs.getBoundingClientRect();
+// 		return {
+// 			x: evt1.clientX - rect.left,
+// 			y: evt1.clientY - rect.top
+// 		};
+// 	}
 
-// // End cache
-
-// function random(min, max) {
-//   if (arguments.length < 2) {
-//     max = min;
-//     min = 0;
-//   }
-  
-//   if (min > max) {
-//     var hold = max;
-//     max = min;
-//     min = hold;
-//   }
-
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// function maxOrbit(x,y) {
-//   var max = Math.max(x,y),
-//       diameter = Math.round(Math.sqrt(max*max + max*max));
-//   return diameter/1;
-// }
-
-// var Star = function() {
-
-//   this.orbitRadius = random(maxOrbit(w,h));
-//   this.radius = random(60, this.orbitRadius) / 12;
-//   this.orbitX = w / 2;
-//   this.orbitY = h / 2;
-//   this.timePassed = random(0, maxStars);
-//   this.speed = random(this.orbitRadius) / 200000;
-//   this.alpha = random(2, 10) / 8;
-
-//   count++;
-//   stars[count] = this;
-// }
-
-// Star.prototype.draw = function() {
-//   var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
-//       y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-//       twinkle = random(10);
-
-//   if (twinkle === 1 && this.alpha > 0) {
-//     this.alpha -= 0.05;
-//   } else if (twinkle === 2 && this.alpha < 1) {
-//     this.alpha += 0.05;
-//   }
-
-//   ctx.globalAlpha = this.alpha;
-//     ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
-//   this.timePassed += this.speed;
-// }
-
-// for (var i = 0; i < maxStars; i++) {
-//   new Star();
-// }
-
-// function animation() {
-//     ctx.globalCompositeOperation = 'source-over';
-//     ctx.globalAlpha = 0.8;
-//     ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 1)';
-//     ctx.fillRect(0, 0, w, h)
-  
-//   ctx.globalCompositeOperation = 'lighter';
-//   for (var i = 1, l = stars.length; i < l; i++) {
-//     stars[i].draw();
-//   };  
-  
-//   window.requestAnimationFrame(animation);
-// }
-
-// animation();
-
-
-
-
-
-
+// 	//Execute when DOM has loaded
+// 	document.addEventListener('DOMContentLoaded',init,false);
+// })();
