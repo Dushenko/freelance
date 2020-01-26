08aa62bfbd0991387f63c65e82032bf17a72c0d6
dushenko <dushenko1992@gmail.com>
2020-01-26 21:17:17 +0200

    add international sectionв

--- Digital/app/blocks/modal/modal.js
+++ Digital/app/blocks/modal/modal.js
@@ -5,6 +5,17 @@ $(document).ready(function () {
 		$('.js-mask').fadeIn();
 	});
 
+	$('.lk-form__modal-btn').click(function(){
+		$('.js-modal-step1').fadeOut();
+		$('.js-modal-step2').fadeIn();
+	});
+
+	$('.lk-form__basket-btn').click(function(){
+		$('.js-modal-step2').fadeOut();
+		$('.js-modal-step3').fadeIn();
+	});
+	
+
 	$('.js-mask').click(function(){
 		$('.modal').fadeOut();
 		$(this).fadeOut();
--- Digital/build/js/app.js
+++ Digital/build/js/app.js
@@ -26,8 +26,16 @@ $(document).ready(function () {
 });
 $(document).ready(function () {
   $('.network').click(function () {
-    $('.js-modal-step2').fadeIn();
+    $('.js-modal-step1').fadeIn();
     $('.js-mask').fadeIn();
+  });
+  $('.lk-form__modal-btn').click(function () {
+    $('.js-modal-step1').fadeOut();
+    $('.js-modal-step2').fadeIn();
+  });
+  $('.lk-form__basket-btn').click(function () {
+    $('.js-modal-step2').fadeOut();
+    $('.js-modal-step3').fadeIn();
   });
   $('.js-mask').click(function () {
     $('.modal').fadeOut();
