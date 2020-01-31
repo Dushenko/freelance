9caea70abfc43e1dd2fa99edcbdfcd3354785459
dushenko <dushenko1992@gmail.com>
2020-01-31 18:45:43 +0200

    add gitignore

--- HerpPatch/js/app.js
+++ HerpPatch/js/app.js
@@ -194,16 +194,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // 	  }
   // 	}, WAIT));
   //   });
+  // $(window).scroll(function () {
+  // 	$('.mov').each(function () {
+  // 		var imagePos = $(this).offset().top;
+  // 		var topOfWindow = $(window).scrollTop();
+  // 		if (imagePos < topOfWindow + 400) {
+  // 			$(this).addClass('anim');
+  // 		}
+  // 	});
+  // });
 
-
-  $(window).scroll(function () {
-    $('.mov').each(function () {
-      var imagePos = $(this).offset().top;
-      var topOfWindow = $(window).scrollTop();
-
-      if (imagePos < topOfWindow + 400) {
-        $(this).addClass('anim');
-      }
-    });
-  });
 });
